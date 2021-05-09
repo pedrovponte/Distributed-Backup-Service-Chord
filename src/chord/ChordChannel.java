0f537e7058d2c8181f51183f80718d89d46e950b
Pedro <up201809694@fe.up.pt>
2021-05-09 11:34:01 +0100

    Correct chord

--- src/chord/ChordChannel.java
+++ src/chord/ChordChannel.java
@@ -1,5 +1,6 @@
 package chord;
 
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
@@ -36,16 +37,62 @@ public class ChordChannel implements Runnable {
                 byte[] message = new byte[65000];
                 this.dis.read(message);
 
-                String messageStr = new String(message, StandardCharsets.ISO_8859_1);
+                handleMessage(message);
 
-                // handle message
-
-                socket.close();
+                this.socket.close();
             }
             catch (Exception e) {
                 System.err.println(e.getMessage());
                 e.printStackTrace();
             }
         }
+    }
+
+    public void sendMessage(InetSocketAddress address, byte[] message) {
+        if(address.equals(this.parent.getNodeInfo().getSocketAddress())) {
+            handleMessage(message);
+            return;
+        }
+
+        try {
+            Socket s = new Socket(address.getAddress(), address.getPort());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeInt(message.length);
+            dos.write(message, 0, message.length);
+            dos.flush();
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void handleMessage(byte[] message) {
+        String[] messageStr = new String(message).split(" ");
+
+        switch (messageStr[0]) {
+            case "JOIN":
+                // JOIN + nodeId + address + port
+                int newNodeId = Integer.parseInt(messageStr[1]);
+                String[] successorInfo = this.node.findSuccessor(this.parent.getNodeInfo().getSocketAddress(), newNodeId);
+
+                break;
+
+            case "FINDSUCC":
+
+
+                break;
+        }
+    }
+
+    public byte[] constructFindSuccessorMessage(int nodeId) {
+        String message = "FINDSUCC " + nodeId.toString() + "\r\n\r\n";
+        return message.getBytes();
+    }
+
+    // JOIN + nodeId + address + port
+    public byte[] constructJoinMessage(int nodeId) {
+        String message = "JOIN " + nodeId + " " + this.parent.getNodeInfo().getIp() + " " + this.parent.getNodeInfo().getPort() + "\r\n\r\n";
+        return nodeId.getBytes();
     }
 }
